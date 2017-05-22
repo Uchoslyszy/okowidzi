be255bfa08e14594fa3aef0457d1b29746fb9c20
szymel <szczesny.szymon@gmail.com>
2017-05-22 18:51:50 +0200

    Stan na dzien 22.05

---
+++ PROGRAM 22.05/GUIObrazy/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QPixmap>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_wczytajobraz_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
